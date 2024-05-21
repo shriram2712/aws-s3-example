e3b561d197e3ca85337e0d882529594f768d5388
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2024-05-21 18:29:58 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_id" {
   description = "The name of the bucket."
-  value       = module.s3-bucket.s3_bucket_id
+  value       = module.s3_bucket.s3_bucket_id
 }
 
 output "s3_bucket_arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = module.s3-bucket.s3_bucket_arn
+  value       = module.s3_bucket.s3_bucket_arn
 }
